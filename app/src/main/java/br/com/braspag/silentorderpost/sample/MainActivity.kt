421a0d7b147738a530dbf7d0e7e11daab761ee13
José Naves <josenaves@gmail.com>
2019-12-11 17:09:55 -0300

    feat: Call silent order post sdk

--- app/src/main/java/br/com/braspag/silentorderpost/sample/MainActivity.kt
+++ app/src/main/java/br/com/braspag/silentorderpost/sample/MainActivity.kt
@@ -3,9 +3,10 @@ package br.com.braspag.silentorderpost.sample
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import br.com.braspag.silentorder.Environment
+import br.com.braspag.silentorder.SilentOrderPost
 import br.com.braspag.silentorderpost.R
 import br.com.braspag.silentorderpost.sample.data.remote.TokenRemoteDatasource
-
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -21,6 +22,8 @@ class MainActivity : AppCompatActivity() {
     private val scope = CoroutineScope(coroutineContext)
 
 
+    val sdk = SilentOrderPost(Environment.SANDBOX)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,24 +34,30 @@ class MainActivity : AppCompatActivity() {
             scope.launch {
                 TokenRemoteDatasource.getAccessToken(
                     "538c04ae-f377-4da8-803b-67c812616387",
-                    onSuccess = { s ->
-                        Snackbar.make(
-                            view,
-                            "Sucesso! AccessToken = $s",
-                            Snackbar.LENGTH_LONG
-                        ).show()
-                    },
-                    onError = { e ->
-                        Snackbar.make(
-                            view,
-                            "Error! code = $e",
-                            Snackbar.LENGTH_LONG
-                        ).show()
-                    }
+                    onSuccess = { s -> silentOrder(s) },
+                    onError = { e -> snackbar("Error! code = $e") }
                 )
-
             }
         }
+    }
+
+    private fun silentOrder(accessToken: String) {
+        snackbar("Sucesso! AccessToken = $accessToken")
+
+        sdk.accessToken = accessToken
+        sdk.call(
+            cardHolderName = "Joselito Barbacena",
+            cardNumber = "4000000000001091",
+            cardExpirationDate = "10/2029",
+            cardCvv = "621",
+            onError = { snackbar(">>>>>> Error - $it") },
+            onSuccess = { snackbar(">>>>>> Success - payment token: ${it.paymentToken}") },
+            onValidation = { snackbar(">>>>>> Validation issue(s) - $it") }
+        )
+    }
+
+    private fun snackbar(message: String) {
+        Snackbar.make(fab.rootView, message, Snackbar.LENGTH_LONG).show()
     }
 
 }
