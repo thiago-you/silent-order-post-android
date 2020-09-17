a40ba2d0838c5a11ef16823bc293369679892520
Maurici <m.f.j@hotmail.com>
2020-09-17 18:52:13 -0300

    Updated ForeignCard to Boolean instead of String

--- silentorder/src/main/java/br/com/braspag/silentorder/SuccessResult.kt
+++ silentorder/src/main/java/br/com/braspag/silentorder/SuccessResult.kt
@@ -5,7 +5,7 @@ import com.squareup.moshi.Json
 data class SuccessResult(
     @Json(name = "PaymentToken") val paymentToken: String,
     @Json(name = "Brand") val brand: String?,
-    @Json(name = "ForeignCard") val foreignCard: String?,
+    @Json(name = "ForeignCard") val foreignCard: Boolean?,
     @Json(name = "BinQueryReturnCode") val binQueryReturnCode: String?,
     @Json(name = "BinQueryReturnMessage") val binQueryReturnMessage: String?
 )
